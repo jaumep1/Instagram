925f4db086508267a7205c04b5f912e9335c2f1f
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-07-06 16:13:41 -0700

    Query Post objects from Parse database

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -8,21 +8,57 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.parse.LogInCallback;
+import com.parse.FindCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private Button btnSubmit;
+    private ImageView ivPostImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        ivPostImage = findViewById(R.id.ivPostImage);
+
+        queryPosts();
+
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                }
+
+                for (Post post : objects) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //Register parse models
+        ParseObject.registerSubclass(Post.class);
+
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("B3lAJKkg67IxBTP8ziEhVYd1bWce49SHGLQ8iFsC")
