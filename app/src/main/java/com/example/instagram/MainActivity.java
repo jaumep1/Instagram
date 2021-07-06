5fe8727f30dda879c7dcdfa8ce957aaf0aced528
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-07-06 14:38:21 -0700

    Added main menu and implemented logout menu item

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            //Compose icon has been clicked
+            Log.d(TAG, "Logout clicked");
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                 @Override
+                 public void done(ParseException e) {
+                     if (e != null) {
+                         Log.e(TAG, "Issue on login", e);
+                         return;
+                     }
+
+                     goLoginActivity();
+                     Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                 }
+             });
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
